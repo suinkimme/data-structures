cf6ba71cd6e42c132b6b5d73a101818b769b172b
Suin Kim <contact@suinkim.me>
2025-06-16 19:02:07 +0900

    Binary  Tree: mirrorTree 함수

--- Data-Structures/Binary_Tree/Q5_E_BT.c
+++ Data-Structures/Binary_Tree/Q5_E_BT.c
@@ -105,7 +105,16 @@ int main()
 
 void mirrorTree(BTNode *node)
 {
-	/* add your code here */
+    if (node == NULL) {
+        return;
+    }
+
+    BTNode *temp = node->right;
+    node->right = node->left;
+    node->left = temp;
+
+    mirrorTree(node->left);
+    mirrorTree(node->right);
 }
 
 //////////////////////////////////////////////////////////////////////////////////
